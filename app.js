80cb65e1342c3b925be2cb157c8269fd22f730ee
ELM-CIEL <mohamed.elmokhtari26@gmail.com>
2025-09-09 10:30:27 +0200

    Commit #2 : Serveur Express ok

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+/* *********************** Serveur Web *************************** */
 'use strict';
 
-console.log('TP CIEL');
+var express = require("express");
+var exp = express();
+
+// Définir le dossier racine pour les fichiers statiques
+exp.use(express.static(__dirname + "/www"));
+
+// Répondre à une requête GET sur la racine
+exp.get("/", function (req, res) {
+    console.log("Reponse à un client");
+    res.sendFile(__dirname + "/www/index.html");
+});
+
+// Traitement des erreurs serveur
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Erreur serveur express");
+});
+
+// Mise en écoute du serveur sur le port 80
+exp.listen(80, function () {
+    console.log("Serveur en ecoute");
+});
